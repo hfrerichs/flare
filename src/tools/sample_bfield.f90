1ab5204ae47bb5c1de4a6815838429afabb63d42
Heinke Frerichs <hfrerichs@wisc.edu>
2014-07-31 11:50:11 -0500

    -using Output_Format to switch between Cartesian and cylindrical coordinates

--- src/tools/sample_bfield.f90
+++ src/tools/sample_bfield.f90
@@ -1,28 +1,48 @@
+!===============================================================================
+! Sample magnetic field
+!
+! Input (taken from run control file):
+!    Grid_File          Sample locations
+!    Output_File
+!    Output_Format      = 1: Cartesian coordinate system (Bx,By,Bz)
+!                       = 2: Cylindrical coordinate system (BR,BZ,Bphi)
+!===============================================================================
 subroutine sample_bfield
+  use run_control, only: Grid_File, Output_File, Output_Format
+  use parallel
   use bfield
-  use run_control, only: Grid_File, Output_File
   use grid
   implicit none
 
-
   integer, parameter :: iu = 42
+
+  real*8  :: Bf(3), xvec(3)
   integer :: iflag
 
-      real*8 :: Bf(3), xvec(3), psi, psi_sepx, psi_axis, R_axis, Z_axis
-      real*8 :: tH(3), eH(3), theta, phi, R, bphi
 
-
-
-  write (6,1000) Output_File
-  call read_grid (Grid_File)
-
+  if (firstP) then
+    write (6, *) 'Sample magnetic field, output in: ', adjustl(trim(Output_File))
+    write (6, *)
+  endif
   open  (iu, file=Output_File, err=5010)
 
+
+  if (Output_Format < 1 .or. Output_Format > 2) then
+     write (6, *) 'undefined output format ', Output_Format
+     stop
+  endif
+  write (iu, 1000) COORDINATES(Output_Format)
+  call read_grid (Grid_File, use_coordinates=COORDINATES(Output_Format))
   grid_point_loop: do
      call get_next_grid_point (iflag, xvec)
      if (iflag.eq.-1) exit grid_point_loop
 
-     Bf = get_Bf_Cart (xvec)
+     select case (Output_Format)
+     case (1)
+        Bf = get_Bf_Cart (xvec)
+     case (2)
+        Bf = get_Bf_Cyl (xvec)
+     end select
 
      Bf = Bf/1.d4	! Gauss -> Tesla
      write (iu,1001) Bf
@@ -30,7 +50,7 @@ subroutine sample_bfield
   close (iu)
 
   return
- 1000 format (3x,'- sample magnetic field, output in: ',a120)
+ 1000 format ('# Magnetic field components [Tesla], coordinate system: ', a12)
  1001 format (3e16.8)
  5010 write (6,5011) Output_File
  5011 format ('error opening file for output: ', a120)
