3b76ea7602ea401ed38adbf368405f31b9b90f0a
Heinke Frerichs <hfrerichs@wisc.edu>
2014-08-04 17:01:23 -0500

    -change output format to e22.14

--- src/tools/sample_bfield.f90
+++ src/tools/sample_bfield.f90
@@ -3,6 +3,7 @@
 !
 ! Input (taken from run control file):
 !    Grid_File          Sample locations
+!
 !    Output_File
 !    Output_Format      = 1: Cartesian coordinate system (Bx,By,Bz)
 !                       = 2: Cylindrical coordinate system (BR,BZ,Bphi)
@@ -21,8 +22,8 @@ subroutine sample_bfield
 
 
   if (firstP) then
-    write (6, *) 'Sample magnetic field, output in: ', adjustl(trim(Output_File))
-    write (6, *)
+     write (6, *) 'Sample magnetic field, output in: ', adjustl(trim(Output_File))
+     write (6, *)
   endif
   open  (iu, file=Output_File, err=5010)
 
@@ -51,7 +52,7 @@ subroutine sample_bfield
 
   return
  1000 format ('# Magnetic field components [Tesla], coordinate system: ', a12)
- 1001 format (3e16.8)
+ 1001 format (3e22.14)
  5010 write (6,5011) Output_File
  5011 format ('error opening file for output: ', a120)
   stop
