6677cb02f150cb43a3ef1a41782997a2c47ce488
Heinke Frerichs <hfrerichs@wisc.edu>
2014-09-03 14:52:15 -0500

    -time measurement for initialization and computation blocks

--- src/main.f90
+++ src/main.f90
@@ -1,4 +1,5 @@
 program main
+  use iso_fortran_env
   use parallel
   use run_control
   use bfield
@@ -6,6 +7,7 @@ program main
   use boundary
   implicit none
 
+  real(real64) :: t1, t2, t3
 
   ! Initialize parallel execution (using MPI)
   call initial_parallel()
@@ -19,13 +21,22 @@ program main
   endif
 
 
+  call cpu_time(t1)
   call load_run_control()
 
   call setup_bfield_configuration()
 
   call setup_boundary()
 
+  call cpu_time(t2)
   call run_control_main()
+  call cpu_time(t3)
+  if (firstP) then
+     write (6, *)
+     write (6, *) 'Time taken for initialization: ', t2-t1, ' seconds'
+     write (6, *) 'Time taken for computation:    ', t3-t2, ' seconds'
+     write (6, *)
+  endif
 
   call finished_parallel()
  1000 format ('========================================================================')
