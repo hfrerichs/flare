f4be87b5c895c19c23c148e8de1860145015290c
Heinke Frerichs <hfrerichs@wisc.edu>
2014-09-10 11:32:50 -0500

    -add optional cut-off surface for separatrix generation -add t_GridPsiN_path

--- src/geometry/separatrix.f90
+++ src/geometry/separatrix.f90
@@ -16,6 +16,12 @@ module separatrix
      procedure :: generate, plot
   end type t_separatrix
 
+  type, extends(t_curve), public :: t_gradPsiN_path
+     contains
+     procedure :: generate => generate_gradPsiN_path
+  end type t_gradPsiN_path
+
+
   contains
 !=======================================================================
 
@@ -35,11 +41,12 @@ module separatrix
 ! theta_cut   =  poloidal cut-off angle (-> split core separatrix into left and right segments)
 !===============================================================================
 
-  subroutine generate (this, Px, orientation, theta_cut)
+  subroutine generate (this, Px, orientation, theta_cut, AltSurf)
   class(t_separatrix)       :: this
   real(real64), intent(in)  :: Px(2)
   integer,      intent(in)  :: orientation
   real(real64), intent(in)  :: theta_cut
+  type(t_curve), intent(in), optional :: AltSurf
 
   real(real64) :: v1(2), v2(2), x0(2), ds, ds0
 
@@ -53,21 +60,21 @@ module separatrix
 
   ! right core segment
   x0 = Px + v1 + v2
-  call this%M1%generate(x0, -1, ds, theta_cut=theta_cut)
+  call this%M1%generate(x0, -1, ds, AltSurf=AltSurf, theta_cut=theta_cut)
 
   ! left core segment
   x0 = Px - v1 + v2
-  call this%M2%generate(x0,  1, ds, theta_cut=theta_cut)
+  call this%M2%generate(x0,  1, ds, AltSurf=AltSurf, theta_cut=theta_cut)
 
   ! right divertor leg
   x0 = Px + v1 - v2
-  call this%M3%generate(x0,  1, ds)
+  call this%M3%generate(x0,  1, ds, AltSurf=AltSurf)
 
   ! left divertor leg
   x0 = Px - v1 - v2
-  call this%M4%generate(x0, -1, ds)
+  call this%M4%generate(x0, -1, ds, AltSurf=AltSurf)
 
-  contains
+  end subroutine generate
 !-----------------------------------------------------------------------
 ! calculate eigenvectors v1,v2 of Hessian matrix of pol. magn. flux at x
 !-----------------------------------------------------------------------
@@ -81,9 +88,9 @@ module separatrix
   ! evaluate Hessian at x
   r(1:2) = x
   r(3)   = 0.d0
-  psi_xx = get_DpsiN(r, 2, 0)
-  psi_xy = get_DpsiN(r, 1, 1)
-  psi_yy = get_DpsiN(r, 0, 2)
+  psi_xx = get_DPsiN(r, 2, 0)
+  psi_xy = get_DPsiN(r, 1, 1)
+  psi_yy = get_DPsiN(r, 0, 2)
 
 
   ! get eigenvalues l1,l2 of Hessian at X-point
@@ -106,7 +113,6 @@ module separatrix
 
   end subroutine H_eigenvectors
 !-----------------------------------------------------------------------
-  end subroutine generate
 !=======================================================================
 
 
@@ -131,5 +137,78 @@ module separatrix
   call this%M4%plot(filename=filename)
   end subroutine plot
 !=======================================================================
+
+
+
+!=======================================================================
+! interface subroutine to normalized grad Psi vector for ODE solver
+!=======================================================================
+  subroutine ePsi_sub (n, t, y, f)
+  use equilibrium
+
+  integer, intent(in)       :: n
+  real(real64), intent(in)  :: t, y(n)
+  real(real64), intent(out) :: f(n)
+
+  real(real64) :: r(3), DPsiDR, DPsiDZ
+
+  if (n .ne. 2) then
+     write (6, *) 'error in subroutine ePsi_sub: n <> 2!'
+     stop
+  endif
+
+  r(1:2) = y
+  r(3)   = 0.d0
+  f(1)   = get_DPsiN(r, 1, 0)
+  f(2)   = get_DPsiN(r, 0, 1)
+  f      = f / sqrt(sum(f**2))
+
+  end subroutine ePsi_sub
+!=======================================================================
+
+
+
+!=======================================================================
+  subroutine generate_gradPsiN_path(this, Px, L)
+  use ode_solver
+  use run_control, only: Trace_Method, N_steps
+  class(t_gradPsiN_path)   :: this
+  real(real64), intent(in) :: Px(2), L
+
+  type(t_ODE)  :: Path
+  real(real64) :: v1(2), v2(2), x0(2), ds, dl, t
+  integer      :: n_seg, is
+
+
+  call H_eigenvectors(Px, v1, v2)
+  ! offset from X-point for tracing
+  dl    = Px(1) / 1.d2
+  x0    = Px - dl*v1
+
+  ! step size
+  ds    = 0.1d0 * dl
+
+  ! expected number of segments
+  n_seg = nint((L-dl) / ds) + 2
+  call this%new(n_seg)
+  this%x_data(0,:) = Px
+  this%x_data(1,:) = x0
+
+
+  ! generate grad PsiN path
+  call Path%init_ODE(2, x0, ds, ePsi_sub, Trace_Method)
+  do is=2,n_seg
+     this%x_data(is,:) = Path%next_step()
+     dl                = dl + ds
+  enddo
+
+
+  ! adjust last node to match L
+  t = (L-dl+ds)/ds
+  this%x_data(n_seg,:) = (1.d0-t)*this%x_data(n_seg-1,:) + t*this%x_data(n_seg,:)
+
+  end subroutine generate_gradPsiN_path
+!=======================================================================
+
 
 end module separatrix
