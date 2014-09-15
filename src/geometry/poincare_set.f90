de13b816b518d7d183e314290ebba376a00aa3e2
Heinke Frerichs <hfrerichs@wisc.edu>
2014-09-15 11:02:51 -0500

    -bugfix for slice numbering -new logical parameter stop_at_boundary for subroutine generate

--- src/geometry/poincare_set.f90
+++ src/geometry/poincare_set.f90
@@ -37,15 +37,16 @@ module poincare_set
 ! nsteps      number of step between slices
 ! solver      id of ODE solver
 !=======================================================================
-  subroutine generate(this, y0, npoints, nsym, nslice, nsteps, solver)
+  subroutine generate(this, y0, npoints, nsym, nslice, nsteps, solver, stop_at_boundary)
   use fieldline
   class(t_poincare_set)    :: this
   real(real64), intent(in) :: y0(3)
   integer,      intent(in) :: npoints, nsym, nslice, nsteps, solver
+  logical,      intent(in) :: stop_at_boundary
 
   type (t_fieldline) :: F
   real(real64)       :: ds, L, theta
-  integer            :: islice, ipoint, istep
+  integer            :: islice, islice0, ipoint, istep
 
 
   ! prepare memory
@@ -66,19 +67,20 @@ module poincare_set
   call F%init(y0, ds, solver, FL_ANGLE)
 
   main_loop: do ipoint=1,npoints
-     slice_loop: do islice=0,nslice-1
+     slice_loop: do islice=1,nslice
         steps: do istep=1,nsteps
            call F%trace_1step()
-           if (F%intersect_boundary()) exit main_loop
+           if (stop_at_boundary  .and.  F%intersect_boundary()) exit main_loop
         enddo steps
 
-        this%slice(islice)%npoints     = ipoint
-        this%slice(islice)%x(ipoint,1) = F%rc(1)
-        this%slice(islice)%x(ipoint,2) = F%rc(2)
+        islice0 = mod(islice,nslice)
+        this%slice(islice0)%npoints     = ipoint
+        this%slice(islice0)%x(ipoint,1) = F%rc(1)
+        this%slice(islice0)%x(ipoint,2) = F%rc(2)
         theta = F%thetac / pi * 180.d0
         if (theta < 0.d0) theta = theta + 360.d0
-        this%slice(islice)%x(ipoint,3) = theta
-        this%slice(islice)%x(ipoint,4) = F%PsiNc
+        this%slice(islice0)%x(ipoint,3) = theta
+        this%slice(islice0)%x(ipoint,4) = F%PsiNc
      enddo slice_loop
   enddo main_loop
 
