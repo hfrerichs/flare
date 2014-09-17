37edd6becaa90e1a15383e9ff65536d1b7ed2a86
Heinke Frerichs <hfrerichs@wisc.edu>
2014-09-17 10:12:41 -0500

    -new subroutines node (return coordinates of node i) and nodes (return number of nodes) for t_grid -implementation of load for unstructured 2D and 3D grids

--- src/core/grid.F90
+++ src/core/grid.F90
@@ -25,8 +25,8 @@ module grid
   integer, public, parameter :: &
      STRUCTURED_2D   = 2, &
      STRUCTURED_3D   = 3, &
-     UNSTRUCTURED_3D = 30, &
-     UNSTRUCTURED_2D = 20
+     UNSTRUCTURED_3D = 6, &
+     UNSTRUCTURED_2D = 5
      !IRREGULAR  = 1, &         ! (x1_i, x2_i, x3_i), i=1..n
            ! (x1_i, x2_j, x3_k), i=1..n1, j=1..n2, k=1..n3
 
@@ -49,6 +49,8 @@ module grid
      procedure :: new_unstructured
      procedure :: load
      procedure :: store
+     procedure :: node                   ! return node coordinates
+     procedure :: nodes                  ! return number of grid nodes
   end type t_grid
 
   contains
@@ -78,33 +80,37 @@ module grid
   endif
 
 
-  select case (layout)
-  case(UNSTRUCTURED_3D)
-     ! unstructured grid nodes (3D)
-     if (allocated(this%x)) deallocate(this%x)
-     allocate (this%x(this%n,3))
-  case(UNSTRUCTURED_2D)
-     ! unstructured grid nodes (2D)
-     if (allocated(this%x)) deallocate(this%x)
-     allocate (this%x(this%n,2))
+  ! allocate memory for grid nodes
+  if (allocated(this%x)) deallocate(this%x)
+  allocate (this%x(this%n,3))
 
-     ! hyperplane reference value
-     if (allocated(this%x3)) deallocate(this%x3)
-     allocate (this%x3(1))
-     this%n3 = 1
-  case(STRUCTURED_2D)
-     if (allocated(this%x1)) deallocate(this%x1)
-     if (allocated(this%x2)) deallocate(this%x2)
-     if (allocated(this%x3)) deallocate(this%x3)
-     allocate (this%x1(this%n1))
-
-     if (.not.present(n2)) then
-        write (6, *) 'error: resolution parameter n2 missing for regular 2D grid!'
-        stop
-     endif
-     allocate (this%x2(this%n2))
-     allocate (this%x3(      1))
-  end select
+!  select case (layout)
+!  case(UNSTRUCTURED_3D)
+!     ! unstructured grid nodes (3D)
+!     if (allocated(this%x)) deallocate(this%x)
+!     allocate (this%x(this%n,3))
+!  case(UNSTRUCTURED_2D)
+!     ! unstructured grid nodes (2D)
+!     if (allocated(this%x)) deallocate(this%x)
+!     allocate (this%x(this%n,2))
+!
+!     ! hyperplane reference value
+!     if (allocated(this%x3)) deallocate(this%x3)
+!     allocate (this%x3(1))
+!     this%n3 = 1
+!  case(STRUCTURED_2D)
+!     if (allocated(this%x1)) deallocate(this%x1)
+!     if (allocated(this%x2)) deallocate(this%x2)
+!     if (allocated(this%x3)) deallocate(this%x3)
+!     allocate (this%x1(this%n1))
+!
+!     if (.not.present(n2)) then
+!        write (6, *) 'error: resolution parameter n2 missing for regular 2D grid!'
+!        stop
+!     endif
+!     allocate (this%x2(this%n2))
+!     allocate (this%x3(      1))
+!  end select
 
 
   end subroutine new
@@ -133,7 +139,146 @@ module grid
   subroutine load(this, filename)
   class(t_grid)                 :: this
   character(len=*), intent(in)  :: filename
+
+  integer, parameter :: iu = 32
+
+
+  if (firstP) then
+     call read_grid()
+  endif
+  call broadcast_grid()
+
+  contains
+!-----------------------------------------------------------------------
+  subroutine read_grid
+
+  character(len=120) :: str
+  real(real64)       :: y3(3), y2(2), y1(1), x0
+  integer :: grid_id, coordinates, layout, fixed_coord, coord1, coord2, i, n
+
+
+  ! 1. open grid file
+  write (6,1000) adjustl(trim(filename))
+  open  (iu, file=filename, err=5000)
+
+
+  ! 2. read grid header and determine grid layout and coordinates
+  read  (iu, 2000) str
+  write (6,2001) str(3:74)
+  if (str(3:9).ne.'grid_id') then
+     write (6,*) 'error: grid type not defined!'
+     stop
+  endif
+  ! 2.1 collect information from grid id
+  read  (str(13:16), '(i4)') grid_id
+  coordinates = grid_id / 100
+  grid_id     = grid_id - 100*coordinates
+  layout      = grid_id / 10
+  fixed_coord = grid_id - 10*layout
+  ! 2.2 setup coordinate indices
+  select case(fixed_coord)
+  case(0)
+  case(1)
+     coord1 = 2; coord2 = 3
+  case(2)
+     coord1 = 1; coord2 = 3
+  case(3)
+     coord1 = 1; coord2 = 2
+  case default
+     write (6, *) 'error: invalid id = ', fixed_coord, ' for fixed coordinate!'
+     stop
+  end select
+
+
+  ! 3. read grid resolution and grid nodes
+  select case(layout)
+  !.....................................................................
+  case (UNSTRUCTURED_3D)
+     ! unstructured 3D grid, n: total number of grid nodes
+     call iscrape (iu, n)
+     call this%new(coordinates, layout, n)
+
+     ! read all grid nodes and convert to cylindrical coordinates
+     do i=1,n
+        read  (iu, *) y3
+
+        call coord_trans (y3, coordinates, y3, CYLINDRICAL)
+        this%x(i,:) = y3
+     enddo
+
+  !.....................................................................
+  case (UNSTRUCTURED_2D)
+     ! unstructured 2D grid, one coordinate is fixed, n: total number of grid nodes
+     call iscrape (iu, n)
+     call this%new(coordinates, layout, n)
+
+     ! read fixed coordinate
+     call rscrape (iu, x0)
+
+     ! read all grid nodes
+     do i=1,n
+        read  (iu, *) y2
+        this%x(i,coord1)      = y2(1)
+        this%x(i,coord2)      = y2(2)
+        this%x(i,fixed_coord) = x0
+     enddo
+  !.....................................................................
+  case default
+     write (6, *) 'error: invalid grid layout ', this%layout, '!'
+     stop
+  end select
+
+
+  ! 4. close grid file
+  close (iu)
+
+
+  ! 5. convert to cylindrical coordinates
+
+
+
+
+  return
+ 1000 format (3x,'- Using grid file: ',a)
+! 1001 format ('progress:             ',i4,' %')
+ 2000 format (a120)
+ 2001 format (8x,a72)
+ 5000 write (6,5001) filename
+ 5001 format ('error reading grid file: ', a120)
+      stop
+  end subroutine read_grid
+!-----------------------------------------------------------------------
+  subroutine broadcast_grid
+  end subroutine broadcast_grid
+!-----------------------------------------------------------------------
   end subroutine load
+!-----------------------------------------------------------------------
+! internal routine to read integer values from header
+!-----------------------------------------------------------------------
+  subroutine iscrape (iu, iout)
+  integer, intent(in)  :: iu
+  integer, intent(out) :: iout
+  character(len=82) :: str
+  read  (iu, 2000) str
+  write (6,2001) str(3:82)
+  read  (str(33:42),*) iout
+ 2000 format (a82)
+ 2001 format (8x,a80)
+  end subroutine iscrape
+!-----------------------------------------------------------------------
+! internal routine to read real values from header
+!-----------------------------------------------------------------------
+  subroutine rscrape (iu, rout)
+  integer,      intent(in)  :: iu
+  real(real64), intent(out) :: rout
+  character(len=82) :: str
+  read  (iu, 2000) str
+  write (6,2001) str(3:82)
+  read  (str(33:42),*) rout
+ 2000 format (a82)
+ 2001 format (8x,a80)
+  end subroutine rscrape
+!-----------------------------------------------------------------------
 !=======================================================================
 
 
@@ -205,6 +350,9 @@ module grid
      do i=1,this%n
         write (iu, 3002) this%x(i,:)
      enddo
+  case default
+     write (6, *) 'to be implemented ...'
+     stop
   end select
 
 
@@ -217,6 +365,39 @@ module grid
  3002 format (2e18.10)
  3003 format (3e18.10)
   end subroutine store
+!=======================================================================
+
+
+
+!=======================================================================
+! return coordinates of node i
+! cylindrical coordinates R[cm], Z[cm], Phi[rad] are used by default
+!=======================================================================
+  function node(this, i, coordinates) result(x)
+  class(t_grid)       :: this
+  integer, intent(in) :: i
+  integer, intent(in), optional :: coordinates
+  real(real64)        :: x(3)
+
+
+  x = this%x(i,:)
+
+  if (present(coordinates)) then
+     call coord_trans (x, CYLINDRICAl, x, coordinates)
+  endif
+
+  end function node
+!=======================================================================
+
+
+
+!=======================================================================
+  function nodes(this) result(n)
+  class(t_grid)       :: this
+  integer             :: n
+
+  n = this%n
+  end function nodes
 !=======================================================================
 
 end module grid
