9911e79f25490dd999b6aa02816efc42fe926744
Heinke Frerichs <hfrerichs@wisc.edu>
2014-10-09 16:11:33 -0500

    -support for parallel execution (based on module dataset)

--- src/tools/sample_bfield.f90
+++ src/tools/sample_bfield.f90
@@ -10,35 +10,49 @@
 !                       = 3: (Output_Format 2), Bpol/Btor
 !===============================================================================
 subroutine sample_bfield
+  use iso_fortran_env
   use run_control, only: Grid_File, Output_File, Output_Format
   use parallel
+  use math
   use bfield
   use equilibrium
   use grid
-  use math
+  use dataset
   implicit none
 
   integer, parameter :: iu = 42
 
-  type(t_grid) :: G
-  real*8  :: Bmod, Bf(3), xvec(3), r(3), PsiN, Bpol, rpol
-  integer :: ig
+  type(t_grid)    :: G
+  type(t_dataset) :: D
+  real(real64)    :: Bmod, Bf(3), xvec(3), r(3), PsiN, Bpol, rpol
+  integer         :: ig, n
 
 
   if (firstP) then
      write (6, *) 'Sample magnetic field, output in: ', adjustl(trim(Output_File))
      write (6, *)
+     open  (iu, file=Output_File, err=5010)
+
+     if (Output_Format < 1 .or. Output_Format > 3) then
+        write (6, *) 'undefined output format ', Output_Format
+        stop
+     endif
+     write (iu, 1000) COORDINATES(min(Output_Format,2))
   endif
-  open  (iu, file=Output_File, err=5010)
 
 
-  if (Output_Format < 1 .or. Output_Format > 3) then
-     write (6, *) 'undefined output format ', Output_Format
-     stop
-  endif
-  write (iu, 1000) COORDINATES(min(Output_Format,2))
+  ! load grid
   call G%load(Grid_File)
-  grid_point_loop: do ig=1,G%nodes()
+
+
+  ! initialize output data
+  n = 5
+  if (Output_Format > 2) n = 6
+  call D%new(G%nodes(), n)
+
+
+  ! main loop
+  grid_loop: do ig=mype+1,G%nodes(),nprs
      xvec = G%node(ig, coordinates=min(Output_Format,2))
 
      select case (Output_Format)
@@ -56,21 +70,27 @@ subroutine sample_bfield
      PsiN = get_PsiN(r)
 
 
-     if (Output_Format .le. 2) then
-        write (iu,1001) Bmod, Bf, PsiN
-     else
+     D%x(ig,1)   = Bmod
+     D%x(ig,2:4) = Bf
+     D%x(ig,5)   = PsiN
+     if (Output_Format > 2) then
         ! ratio of poloidal to toroidal field
         Bpol = sqrt(Bf(1)**2 + Bf(2)**2)
         rpol = Bpol / Bf(3)
-        write (iu,1002) Bmod, Bf, PsiN, rpol
+        D%x(ig,6)   = rpol
      endif
-  enddo grid_point_loop
-  close (iu)
+  enddo grid_loop
+  call D%mpi_allreduce()
+
+
+  ! write data
+  if (firstP) then
+     call D%plot(iu, formatstr='e22.14')
+     close (iu)
+  endif
 
   return
  1000 format ('# Magnetic field components [Tesla], coordinate system: ', a12)
- 1001 format (5e22.14)
- 1002 format (6e22.14)
  5010 write (6,5011) Output_File
  5011 format ('error opening file for output: ', a120)
   stop
