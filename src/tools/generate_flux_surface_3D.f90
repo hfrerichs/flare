834801812e054ae7f2fef621a94f2e8062c0033d
Heinke Frerichs <hfrerichs@wisc.edu>
2014-10-22 10:59:20 -0500

    -parameter Output_Format allows to choose between poloidal angle and distance along flux surface slice for sorting and sampling points

--- src/tools/generate_flux_surface_3D.f90
+++ src/tools/generate_flux_surface_3D.f90
@@ -6,13 +6,15 @@
 ! N_points              Number of points for each slice
 ! N_sym                 Toroidal symmetry number (i.e. 5: 0->72 deg)
 ! N_mult                Number of slices
+! Output_Format         = 1: use poloidal angle to sample points of flux surface slice
+!                       = 2: use distance along flux surface slice to sample points
 !
 ! Trace_Method -> see e.g. connection length
 ! Output_File
 !===============================================================================
 subroutine generate_flux_surface_3D
   use iso_fortran_env
-  use run_control, only: x_start, N_points, N_sym, N_mult, N_steps, Trace_Method, Output_File
+  use run_control, only: x_start, N_points, N_sym, N_mult, N_steps, Trace_Method, Output_File, Output_Format
   use flux_surface_3D
   use math
   use parallel
@@ -35,7 +37,7 @@ subroutine generate_flux_surface_3D
   y0    = x_start
   y0(3) = y0(3) / 180.d0 * pi
   if (N_steps == 0) N_steps = 10
-  call S%generate(y0, N_points, N_sym, N_mult, N_steps, Trace_Method)
+  call S%generate(y0, N_points, N_sym, N_mult, N_steps, Trace_Method, poloidal_coordinate=Output_Format)
   call S%plot(filename=Output_File)
   
  1001 format (8x,'Toroidal symmetry number:         ',i4)
