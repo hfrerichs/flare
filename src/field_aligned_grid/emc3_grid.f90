ecc98c00df33d9438570f2400fbfe852f5ffddad
Heinke Frerichs <hfrerichs@wisc.edu>
2015-02-18 11:45:48 -0600

    -add debugging of plate generation -bugfix for node indices for calculating cell center

--- src/field_aligned_grid/emc3_grid.f90
+++ src/field_aligned_grid/emc3_grid.f90
@@ -658,6 +658,9 @@ end module emc3_grid
   use boundary
   use curve2D
   use math
+  use run_control, only: Debug
+  use string
+  use dataset
   implicit none
 
   integer, parameter :: iu = 78
@@ -665,16 +668,24 @@ end module emc3_grid
 
   type(t_curve), dimension(:,:), allocatable :: C
 
+  real(real64), dimension(:), allocatable   :: RC_TEM, ZC_TEM
+  integer, dimension(:), allocatable   :: ID_TEM
   integer, dimension(:,:), allocatable :: iindex ! number of cells behind a plate
   integer, dimension(:), allocatable   :: knumb  ! cell index in flux tube for plate cells
 
   logical      :: plate_cell
   real(real64) :: x(3), phi
-  integer      :: nr, np, nt, iz, i, j, k, l, l1, l2, irun, icut, ig(8)
+  integer      :: nr, np, nt, iz, i, j, k, l, l1, l2, irun, icut, ig(8), ic
 
 
+  !call outside_boundary_check()
+  allocate (ID_TEM(0:MESH_P_OS(NZONET)-1))
+  allocate (RC_TEM(0:MESH_P_OS(NZONET)-1))
+  allocate (ZC_TEM(0:MESH_P_OS(NZONET)-1))
+  ID_TEM = 0
   open  (iu, file='plates.dat')
   do iz=0,NZONET-1
+  !do iz=0,0
      nr = ZON_RADI(iz)
      np = ZON_POLO(iz)
      nt = ZON_TORO(iz)
@@ -692,6 +703,11 @@ end module emc3_grid
          phi  = phi / 180.d0 * pi          !!!!! TODO: consistent units for PHI_PLANE
          phi  = phi_sym(phi, N_sym)
          C(k,l) = S_quad(l)%slice(phi)
+
+         if (Debug) then
+            call C(k,l)%plot(filename='debug/Q4surf_'//trim(str(l))//'_zone'//trim(str(iz))// &
+                                      '_t'//trim(str(k)))
+         endif
      enddo
      enddo
 
@@ -700,24 +716,35 @@ end module emc3_grid
      do irun=0,1
         icut = 0
         ! loop over all flux tubes
-        do i=0,nr-1
-        do j=0,np-1
+        do i=R_SURF_PL_TRANS_RANGE(1,iz),R_SURF_PL_TRANS_RANGE(2,iz)-1
+        do j=P_SURF_PL_TRANS_RANGE(1,iz),P_SURF_PL_TRANS_RANGE(2,iz)-1
+        !do i=0,nr-1
+        !do j=0,np-1
+        !do i=23,23
+        !do j=120,120
         do k=0,nt-1
            ig(1)   = i + (j + k*SRF_POLO(iz))*SRF_RADI(iz) + GRID_P_OS(iz)
            ig(2)   = ig(1) + 1
-           ig(4)   = ig(2) + SRF_RADI(iz)
-           ig(3)   = ig(4) + 1
+           ig(3)   = ig(2) + SRF_RADI(iz)
+           ig(4)   = ig(3) - 1
            ig(5:8) = ig(1:4) + SRF_POLO(iz)*SRF_RADI(iz)
 
            x(1)    = sum(RG(ig))/8.d0
            x(2)    = sum(ZG(ig))/8.d0
            x(3)    = (PHI_PLANE(k+1+PHI_PL_OS(iz)) + PHI_PLANE(k+PHI_PL_OS(iz))) / 2.d0
+           !write (6, *) x
+           x(3)    = x(3) / 180.d0 * pi
            x(3)    = phi_sym(x(3), N_sym)
+
+           ic      = i + (j + k*ZON_POLO(iz))*ZON_RADI(iz) + MESH_P_OS(iz)
+           RC_TEM(ic) = x(1)
+           ZC_TEM(ic) = x(2)
 
            !plate_cell = .false.
            plate_cell = outside_boundary()
 
            if (plate_cell) then
+              ID_TEM(ic) = 1
               icut = icut + 1
               if (irun == 1) then
                  iindex(i,j) = iindex(i,j) + 1
@@ -763,6 +790,32 @@ end module emc3_grid
   enddo
   close (iu)
 
+
+  ! additional output for debugging
+  if (Debug) then
+     do iz=0,NZONET-1
+     do k=0,ZON_TORO(iz)-1
+        open  (99, file='debug/SlicePlasma_zone'//trim(str(iz))//'_t'//trim(str(k))//'.plt')
+        open  (98, file='debug/SliceBoundary_zone'//trim(str(iz))//'_t'//trim(str(k))//'.plt')
+
+        do i=R_SURF_PL_TRANS_RANGE(1,iz),R_SURF_PL_TRANS_RANGE(2,iz)-1
+        do j=P_SURF_PL_TRANS_RANGE(1,iz),P_SURF_PL_TRANS_RANGE(2,iz)-1
+           ic      = i + (j + k*ZON_POLO(iz))*ZON_RADI(iz) + MESH_P_OS(iz)
+           if (ID_TEM(ic) == 1) then
+              write (98, *) RC_TEM(ic), ZC_TEM(ic)
+           else
+              write (99, *) RC_TEM(ic), ZC_TEM(ic)
+           endif
+        enddo
+        enddo
+        close (99)
+        close (98)
+     enddo
+     enddo
+
+     call plate_check_all()
+  endif
+  deallocate (ID_TEM, RC_TEM, ZC_TEM)
   contains
   !-------------------------------------------------------------------
   function outside_boundary()
@@ -802,6 +855,159 @@ end module emc3_grid
   enddo
 
   end function outside_boundary
+  !-------------------------------------------------------------------
+
+
+  !-------------------------------------------------------------------
+  subroutine outside_boundary_check
+  use field_aligned_grid, only: N_sym
+
+  integer, parameter :: iu = 99, iu2 = 98, n = 101
+
+  character(len=72) :: s
+  real(real64) :: x(2), Phi
+  integer :: i
+
+  allocate (C(0:n-1, n_quad))
+  open  (iu, file='fl+.dat')
+  open  (iu2, file='fl+B.dat')
+  read  (iu, *) s
+  do i=0,n-1
+     read (iu, *) x, Phi
+
+     phi  = phi_sym(phi, N_sym)
+     C(i,1) = S_quad(1)%slice(phi)
+
+     if (C(i,1)%outside(x)) then
+        !write (iu2, *) Phi, 1
+     else
+        !write (iu2, *) Phi, 0
+        write (iu2, *) x, Phi
+     endif
+  enddo
+  close (iu)
+  close (iu2)
+
+  stop
+  end subroutine outside_boundary_check
+  !-------------------------------------------------------------------
+
+
+  !-------------------------------------------------------------------
+  subroutine plate_check_all
+  integer :: iz, k, j, i, ic1, ic2, iplate
+
+
+  write (6, *) 'running plate checks ...'
+
+  ID_TEM = ID_TEM*2 - 1
+  iplate = 0
+
+  do iz=0,NZONET-1
+  do i=0,ZON_RADI(iz)-1
+  do j=0,ZON_POLO(iz)-1
+     do k=0,ZON_TORO(iz)-2
+        ic1 = i + (j +  k   *ZON_POLO(iz))*ZON_RADI(iz) + MESH_P_OS(iz)
+        ic2 = i + (j + (k+1)*ZON_POLO(iz))*ZON_RADI(iz) + MESH_P_OS(iz)
+
+        if (ID_TEM(ic1)*ID_TEM(ic2) < 0) then
+           iplate = iplate + 1
+           write (6, *) iplate
+           call plate_check(iz,i,j,k+1)
+        endif
+     enddo
+  enddo
+  enddo
+  enddo
+
+  end subroutine plate_check_all
+  !-------------------------------------------------------------------
+  subroutine plate_check(iz, ir, ip, jt)
+  use Q4
+  use math
+  use grid
+  use fieldline
+  use dataset
+  integer, intent(in) :: iz, ir, ip, jt
+
+  real(real64), parameter :: Limit = 360.d0
+
+  type(t_Q4)   :: Q
+  type(t_grid) :: G
+  type(t_fieldline) :: F
+  type(t_dataset)   :: D
+  real(real64) :: x1(2), x2(2), x3(2), x4(2), phi, y(3), ts, Lc, Lcsav
+  integer      :: ig(4), i, idir, n, nsuccess
+
+
+  ! calculate node indices
+  ig(1)   = ir + (ip + jt*SRF_POLO(iz))*SRF_RADI(iz) + GRID_P_OS(iz)
+  ig(2)   = ig(1) + 1
+  ig(3)   = ig(2) + SRF_RADI(iz)
+  ig(4)   = ig(3) - 1
+
+  ! get node coordinates
+  x1(1)   = RG(ig(1)); x1(2)   = ZG(ig(1))
+  x2(1)   = RG(ig(2)); x2(2)   = ZG(ig(2))
+  x3(1)   = RG(ig(3)); x3(2)   = ZG(ig(3))
+  x4(1)   = RG(ig(4)); x4(2)   = ZG(ig(4))
+  phi     = PHI_PLANE(jt+PHI_PL_OS(iz)) / 180.d0 * pi
+
+  ! setup quadrilateral
+  call Q%set_nodes(x1, x2, x3, x4)
+
+  ! generate mesh
+  n = 10
+  G = Q%generate_mesh(n, n, phi)
+  G%coordinates       = CYLINDRICAL
+  !G%fixed_coord       = 3
+  !G%fixed_coord_value = phi / 180.d0 * pi
+
+  ! sample connection length on grid
+  ts       = 1.d0
+  Lcsav    = 0.d0
+  nsuccess = 0
+  call D%new(G%nodes(),5)
+  do i=1,G%nodes()
+     y = G%node(i)
+
+     D%x(i,1) = y(1)
+     D%x(i,2) = y(2)
+
+     ! trace field line in both directions
+     do idir=-1,1,2
+        call F%init(y, idir*ts, NM_AdamsBashforth4, FL_ARC)
+
+        trace_loop: do
+           call F%trace_1step()
+           Lc = F%phi_int * 180.d0 / pi
+
+           if (abs(Lc) > Limit) exit trace_loop
+
+           if (F%intersect_boundary()) exit trace_loop
+        enddo trace_loop
+
+        D%x(i,3 + (idir+1)/2) = Lc
+     enddo
+     D%x(i,5) = min(abs(D%x(i,3)), abs(D%x(i,4)))
+
+     ! success frequency, average shortest toroidal distance
+     if (D%x(i,5) < Limit) then
+        nsuccess = nsuccess + 1
+        Lcsav    = Lcsav    + D%x(i,5)
+     endif
+  enddo
+  Lcsav = Lcsav / nsuccess
+  write (6, *) 'success frequency = ', 100.d0 * nsuccess / G%nodes(), ' %'
+  write (6, *) 'average toroidal distance to plates [deg] = ', Lcsav
+  call G%store('plate1.grid')
+  call D%plot(filename='plate1.dat')
+
+  call D%destroy()
+  call G%destroy()
+  stop
+
+  end subroutine plate_check
   !-------------------------------------------------------------------
   end subroutine generate_plates
 !=======================================================================
