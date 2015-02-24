bbeac863aae274f49e1b892e3af27d9521342417
Heinke Frerichs <hfrerichs@wisc.edu>
2015-02-24 11:42:26 -0600

    -remove compilation of hyperbolic_fixed_point from master

--- src/development/run_control_development.f90
+++ src/development/run_control_development.f90
@@ -6,8 +6,8 @@ subroutine run_control_development (Run_Type)
 
 
   select case (Run_Type)
-  case ('hyperbolic_fixed_point')
-     call hyperbolic_fixed_point
+!  case ('hyperbolic_fixed_point')
+!     call hyperbolic_fixed_point
   case default
      write (6, *) 'run type "', trim(Run_Type), '" not defined!'
      stop
