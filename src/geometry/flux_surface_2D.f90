82c0301387e20f48f8d9573a3b7a56adee764584
Heinke Frerichs <hfrerichs@wisc.edu>
2015-05-20 11:55:15 -0500

    -reduce automatic trace step (should be okay unless very close to the separatrix)

--- src/geometry/flux_surface_2D.f90
+++ src/geometry/flux_surface_2D.f90
@@ -64,7 +64,8 @@ module flux_surface_2D
   if (present(Trace_Step)) then
      ds = Trace_Step
   else
-     ds = length_scale() / 10000.d0
+     ! TODO: get PsiN from equilibrium and use small trace step close to the separatrix
+     ds = length_scale() / 800.d0
   endif
 
 
