756219bddf7acd4c998ccb3aa8395ceb3ffde597
Heinke Frerichs <hfrerichs@wisc.edu>
2015-07-16 15:37:49 -0500

    -module emc3_grid: add subroutines load_bfstren, flux_tube_length, RZ_REAL_COORDINATES, reconstruct_field_line and check_mesh -add FLR_analysis subroutines grid_accuracy and flux_conservation

--- src/fieldline_grid/emc3_grid.F90
+++ src/fieldline_grid/emc3_grid.F90
@@ -196,6 +196,25 @@ module emc3_grid
 
 
 !=======================================================================
+  subroutine load_bfstren()
+  implicit none
+
+  integer, parameter :: iu = 24
+
+
+  if (allocated(BFSTREN)) deallocate(BFSTREN)
+  allocate (BFSTREN(0:GRID_P_OS(NZONET)-1))
+
+  open  (iu, file='bfield.dat')
+  read  (iu, *) BFSTREN
+  close (iu)
+
+  end subroutine load_bfstren
+!=======================================================================
+
+
+
+!=======================================================================
 ! WRITE_EMC3_GRID (write 3D field aligned grid to file "grid3D.dat")
 !=======================================================================
   subroutine write_emc3_grid
@@ -230,8 +249,8 @@ module emc3_grid
 
 
 
-!=======================================================================
 #ifdef FLARE
+!=======================================================================
   function export_flux_tube(iz, ir, ip) result(flux_tube)
   use flux_tube
   use math
@@ -284,8 +303,232 @@ module emc3_grid
   enddo
 
   end subroutine export_slice
-#endif
 !=======================================================================
+
+
+
+!=======================================================================
+  function flux_tube_length(iz, ir, ip) result(L)
+  use iso_fortran_env
+  integer, intent(in) :: iz, ir, ip
+  real(real64)        :: L
+
+  real(real64) :: R1, Z1, phi1, R2, Z2, phi2, dl
+  integer      :: it, ig(4)
+
+            ! get length of finite, interpolated field line
+  L = 0.d0
+  do it=0,ZON_TORO(iz)-1
+     ig(1) = ir+(ip+it*SRF_POLO(iz))*SRF_RADI(iz)+GRID_P_OS(iz)
+     ig(2) = ig(1) + SRF_RADI(iz)
+     ig(3) = ig(2) + 1
+     ig(4) = ig(1) + 1
+
+     R2    = 0.25d0 * sum(RG(ig))
+     Z2    = 0.25d0 * sum(ZG(ig))
+     phi2  = PHI_PLANE(PHI_PL_OS(iz)+it+1)
+
+     if (it > 0) then
+        dl = dsqrt((0.5d0*(R1+R2)*(phi2-phi1))**2  +  (R2-R1)**2  +  (Z2-Z1)**2)
+        L  = L + dl
+     endif
+
+     R1    = R2
+     Z1    = Z2
+     phi1  = phi2
+  enddo
+
+  end function flux_tube_length
+!=======================================================================
+
+
+
+!=======================================================================
+      SUBROUTINE RZ_REAL_COORDINATES(NZ0,JR0,JP0,RJ0,PJ0,TJ0,R,Z)
+
+      INTEGER,INTENT(IN) :: NZ0,JR0,JP0
+      REAL*8, INTENT(IN) :: RJ0,PJ0,TJ0
+      REAL*8, INTENT(OUT):: R,Z
+
+      INTEGER :: JT0, I1, I2, I3, I4
+      REAL*8  :: AR, BR, CR, DR, AZ, BZ, CZ, DZ, R1, Z1, R2, Z2, XT
+      JT0 = TJ0
+      XT  = TJ0 - JT0
+
+      I1= JR0+(JP0+JT0*SRF_POLO(NZ0))*SRF_RADI(NZ0)+GRID_P_OS(NZ0)
+      I2= I1 + SRF_RADI(NZ0)
+      I3= I2 + 1
+      I4= I1 + 1
+
+      AR  = 0.25*(RG(I1)+RG(I2)+RG(I3)+RG(I4))
+      BR  = 0.25*(RG(I3)+RG(I4)-RG(I1)-RG(I2))
+      CR  = 0.25*(RG(I3)+RG(I2)-RG(I1)-RG(I4))
+      DR  = 0.25*(RG(I1)+RG(I3)-RG(I2)-RG(I4))
+
+      AZ  = 0.25*(ZG(I1)+ZG(I2)+ZG(I3)+ZG(I4))
+      BZ  = 0.25*(ZG(I3)+ZG(I4)-ZG(I1)-ZG(I2))
+      CZ  = 0.25*(ZG(I3)+ZG(I2)-ZG(I1)-ZG(I4))
+      DZ  = 0.25*(ZG(I1)+ZG(I3)-ZG(I2)-ZG(I4))
+      R1  = AR + BR*RJ0 + CR*PJ0 + DR*RJ0*PJ0
+      Z1  = AZ + BZ*RJ0 + CZ*PJ0 + DZ*RJ0*PJ0
+
+      IF ( XT == 0. ) THEN
+        R   = R1
+        Z   = Z1
+        RETURN
+      ENDIF
+
+      I1= I1 + SRF_POLO(NZ0)*SRF_RADI(NZ0)
+      I2= I1 + SRF_RADI(NZ0)
+      I3= I2 + 1
+      I4= I1 + 1
+
+      AR  = 0.25*(RG(I1)+RG(I2)+RG(I3)+RG(I4))
+      BR  = 0.25*(RG(I3)+RG(I4)-RG(I1)-RG(I2))
+      CR  = 0.25*(RG(I3)+RG(I2)-RG(I1)-RG(I4))
+      DR  = 0.25*(RG(I1)+RG(I3)-RG(I2)-RG(I4))
+
+      AZ  = 0.25*(ZG(I1)+ZG(I2)+ZG(I3)+ZG(I4))
+      BZ  = 0.25*(ZG(I3)+ZG(I4)-ZG(I1)-ZG(I2))
+      CZ  = 0.25*(ZG(I3)+ZG(I2)-ZG(I1)-ZG(I4))
+      DZ  = 0.25*(ZG(I1)+ZG(I3)-ZG(I2)-ZG(I4))
+
+      R2  = AR + BR*RJ0 + CR*PJ0 + DR*RJ0*PJ0
+      Z2  = AZ + BZ*RJ0 + CZ*PJ0 + DZ*RJ0*PJ0
+
+      R   = R1 + (R2-R1)*XT
+      Z   = Z1 + (Z2-Z1)*XT
+      RETURN
+      END SUBROUTINE RZ_REAL_COORDINATES
+!=======================================================================
+
+
+
+!=======================================================================
+  subroutine reconstruct_field_line(iz, ir, ip, rj, pj, x)
+  use iso_fortran_env
+  integer,      intent(in)  :: iz, ir, ip
+  real(real64), intent(in)  :: rj, pj
+  real(real64), intent(out) :: x(0:ZON_TORO(iz),3)
+
+  real(real64) :: R, Z, tj
+  integer :: it
+
+
+  do it=0,ZON_TORO(iz)
+     tj      = 1.d0 * it
+     call RZ_REAL_COORDINATES(iz, ir, ip, rj, pj, tj, R, Z)
+     x(it,1) = R
+     x(it,2) = Z
+     x(it,3) = PHI_PLANE(it + PHI_PL_OS(iz))
+  enddo
+
+  end subroutine reconstruct_field_line
+!=======================================================================
+
+
+
+!=======================================================================
+  subroutine check_mesh(iz, div, NL, pitch)
+  use iso_fortran_env
+  implicit none
+
+  integer,      intent(in)  :: iz
+  real(real64), intent(out) :: div(0:ZON_POLO(iz)-1, 0:ZON_RADI(iz)-1)
+  real(real64), intent(out) :: pitch(0:ZON_POLO(iz)-1, 0:ZON_RADI(iz)-1)
+  real(real64), intent(out) :: NL(0:ZON_POLO(iz)-1, 0:ZON_RADI(iz)-1, 0:ZON_TORO(iz))
+
+      INTEGER :: I,J,K,I1,I2,I3,I4,IS,IC,IG
+      REAL*8  :: A1,A2,B1,B2,C1,C2,D1,D2,D,ABCD1,ABCD2, &
+                 NON_LINEAR,NON_LINEAR_AVER, &
+                 AREA1,AREA2,R1,R2,Z1,Z2,BF1,BF2,FI1,FI2,DFL, &
+                 AVER_F,DIVMAX,DIVAVE,F_SUM    
+      REAL*8,DIMENSION(:  ),ALLOCATABLE :: FLUX,DIV_CELL
+      LOGICAL :: S_S,PROB                      
+
+      DIVMAX = 0.
+      DIVAVE = 0.
+      F_SUM  = 0.
+      div    = 0.d0
+      NL     = 0.d0
+      NON_LINEAR_AVER = 0.
+
+      ALLOCATE(FLUX(ZON_TORO(IZ)))
+
+      DO 10 I =0,ZON_RADI(IZ)-1
+      DO 10 J =0,ZON_POLO(IZ)-1
+      DO 20 K =0,ZON_TORO(IZ)
+         I1 = I + (J+K*SRF_POLO(IZ))*SRF_RADI(IZ) + GRID_P_OS(IZ)
+         I2 = I1+ SRF_RADI(IZ)
+         I3 = I2 + 1
+         I4 = I1 + 1
+
+         ABCD1 = (RG(I3)-RG(I2))*(ZG(I2)-ZG(I1)) - (RG(I2)-RG(I1))*(ZG(I3)-ZG(I2))
+         ABCD2 = (RG(I4)-RG(I1))*(ZG(I3)-ZG(I4)) - (RG(I3)-RG(I4))*(ZG(I4)-ZG(I1))
+
+         A1 = 0.25*(RG(I3)+RG(I4)-RG(I1)-RG(I2))
+         B1 = 0.25*(RG(I3)+RG(I2)-RG(I1)-RG(I4))
+         C1 = 0.25*(RG(I1)+RG(I3)-RG(I2)-RG(I4))
+
+         A2 = 0.25*(ZG(I3)+ZG(I4)-ZG(I1)-ZG(I2))
+         B2 = 0.25*(ZG(I3)+ZG(I2)-ZG(I1)-ZG(I4))
+         C2 = 0.25*(ZG(I1)+ZG(I3)-ZG(I2)-ZG(I4))
+
+         D  = ABS(A1*B2 - A2*B1)
+
+         D1 = -(C1*B2-C2*B1)
+         D2 = -(A1*C2-A2*C1)
+         NON_LINEAR = ABS(D1)+ABS(D2)
+         NL(J,I,K) = NON_LINEAR/D
+         NON_LINEAR_AVER = NON_LINEAR_AVER + NON_LINEAR/D
+
+! check flux conservation in plasma transport range
+
+         IF(I>=R_SURF_PL_TRANS_RANGE(1,IZ) .AND. I< R_SURF_PL_TRANS_RANGE(2,IZ) .AND. &
+            J>=P_SURF_PL_TRANS_RANGE(1,IZ) .AND. J< P_SURF_PL_TRANS_RANGE(2,IZ) ) THEN 
+
+            AREA2=0.5*ABS(ABCD1+ABCD2)
+            R2   = 0.25*(RG(I1)+RG(I2)+RG(I3)+RG(I4))
+            Z2   = 0.25*(ZG(I1)+ZG(I2)+ZG(I3)+ZG(I4))
+            FI2  = PHI_PLANE(PHI_PL_OS(IZ)+K)
+            BF2  = 0.25*( BFSTREN(I1)+BFSTREN(I2)+BFSTREN(I3)+BFSTREN(I4) )
+
+            IF(K/=0) THEN
+             DFL    = 0.5*(R1+R2)*ABS(FI2-FI1)
+             PITCH(J,I)  = DFL/SQRT(DFL**2+(R2-R1)**2+(Z2-Z1)**2)
+             FLUX(K)= (AREA1+AREA2)*PITCH(J,I)*(BF2+BF1)*0.25
+            ENDIF
+            AREA1=AREA2; R1=R2; Z1=Z2; FI1=FI2; BF1=BF2
+         ENDIF
+20    CONTINUE
+      IF(I>=R_SURF_PL_TRANS_RANGE(1,IZ) .AND. I< R_SURF_PL_TRANS_RANGE(2,IZ) .AND. &
+         J>=P_SURF_PL_TRANS_RANGE(1,IZ) .AND. J< P_SURF_PL_TRANS_RANGE(2,IZ) ) THEN
+         AVER_F = SUM(FLUX)/FLOAT(ZON_TORO(IZ))
+         DIV(J,I) = (MAXVAL(FLUX)-MINVAL(FLUX))/AVER_F
+         DIVMAX = MAX(DIVMAX,DIV(J,I))
+         DIVAVE = DIVAVE+MAXVAL(FLUX)-MINVAL(FLUX)   
+         F_SUM  = F_SUM + AVER_F
+      ENDIF
+10    CONTINUE
+      DEALLOCATE(FLUX)
+
+
+      NON_LINEAR_AVER = NON_LINEAR_AVER/FLOAT(MESH_P_OS(NZONET))
+      WRITE(6,*)' 1.1 mesh shape '
+      WRITE(6,'(A18,F8.3,A2)')'   Non-linearity:', NON_LINEAR_AVER*100.,' %'
+
+      DIVAVE = DIVAVE/(F_SUM+1.E-10)*100.
+      WRITE(6,*) ' 1.2 Check accuracy in flux conservation'
+      WRITE(6,'(A18,F8.3,A2)')'   Flux conserved:',100.-DIVAVE,' %'
+      WRITE(6,'(A18,F8.3,A2)')'   max. diviation:',DIVMAX*100.,' %'
+      IF(DIVAVE>1. .OR. DIVMAX>0.1) THEN
+        WRITE(6,*)
+        WRITE(6,*)' Mag. flux not conserved inside flux tubes'
+      ENDIF
+
+  end subroutine check_mesh
+!=======================================================================
+#endif
 
 
 
--- src/tools/FLR_analysis.f90
+++ src/tools/FLR_analysis.f90
@@ -1,12 +1,8 @@
 !===============================================================================
 ! Field line reconstruction analysis
-!
-! Input (taken from run control file):
-!    Output_File        Configuration file for field line analysis
 !===============================================================================
 subroutine FLR_analysis
   use iso_fortran_env
-  use run_control, only: Target_File => Output_File
   use parallel
   use flux_tube
   use dataset
@@ -17,6 +13,7 @@ subroutine FLR_analysis
 
   character(len=72)  :: &
      Operation     = 'nothing', &
+     Target_File   = 'analysis.conf', &
      Output_File   = 'output.dat', &
      Grid_File     = 'output.grid', &
      Cross_Section = ''
@@ -50,6 +47,10 @@ subroutine FLR_analysis
      call single_fieldline
   case ('fluxtube')
      call fluxtube
+  case ('grid_accuracy')
+     call grid_accuracy()
+  case ('flux_conservation')
+     call flux_conservation()
   case default
      write (6, *) 'error: invalid operation ', trim(Operation), '!'
      stop
@@ -183,6 +184,187 @@ subroutine FLR_analysis
 
  1000 format(3x,'- Analyzing flux tube')
   end subroutine fluxtube
+!=======================================================================
+
+
+
+!=======================================================================
+  subroutine grid_accuracy()
+  use emc3_grid
+  use fieldline
+  use equilibrium
+  use dataset
+  use grid
+  implicit none
+
+  type(t_fieldline) :: Fo
+  type(t_dataset)   :: D
+  type(t_grid)      :: G1, G2
+  real(real64), dimension(:,:), allocatable :: Fg
+  integer, dimension(:), allocatable :: ir_list, ip_list
+
+  real(real64) :: Dphi, xi(3), xo(3), ts, dx(-1:1), dr(-1:1), dt(-1:1), xg(3)
+  real(real64) :: dPsidR, dPsidZ, Psio, Psig, dPsi, thetao, thetag, dtheta
+  integer :: i, j, ig, n, idir, it(-1:1), iz, tm, tc, ierr, iscan
+
+
+  write (6, 1000)
+  call load_emc3_grid()
+
+
+  ts = pi2 / 3600.d0
+  tm = NM_AdamsBashforth4
+  tc = FL_ANGLE
+
+
+  iz    = 0
+  iscan = 3
+  select case(iscan)
+  ! set up poloidal profile
+  case(1)
+     n  = ZON_POLO(iz)
+     allocate (ip_list(n), ir_list(n))
+     ir_list = 6
+     !ir_list = ZON_RADI(0) - 4
+     do i=1,ZON_POLO(iz)
+        ip_list(i) = i-1
+     enddo
+
+  ! set up radial-poloidal scan
+  case(3)
+     n  = ZON_POLO(iz) * ZON_RADI(iz)
+     ig = 1
+     allocate (ip_list(n), ir_list(n))
+     do i=0,ZON_RADI(iz)-1
+        do j=0,ZON_POLO(iz)-1
+           ip_list(ig) = j
+           ir_list(ig) = i
+           ig          = ig + 1
+        enddo
+     enddo
+
+  end select
+
+
+  ! Fo: field line from (o)rdinary differential equation
+  ! Fg: field line reconstructed from (g)rid
+  allocate (Fg(0:ZON_TORO(iz),3))
+  call D%new(n, 7)
+  call G1%new(LOCAL, UNSTRUCTURED, FIXED_COORD3, n)
+  call G2%new(LOCAL, UNSTRUCTURED, FIXED_COORD3, n)
+  it(-1) = 0
+  it( 0) = ZON_TORO(iz) / 2
+  it( 1) = ZON_TORO(iz)
+  do i=1,n
+     write (6, *) ir_list(i), ip_list(i)
+     call reconstruct_field_line (iz, ir_list(i), ip_list(i), 0.d0, 0.d0, Fg)
+     ! initial point for field line tracing
+     xi = Fg(it(0),:)
+
+     do idir=-1,1,2
+        Dphi = abs(Fg(it(idir), 3) - Fg(it(0), 3))
+        call Fo%init(xi, idir*ts, tm, tc)
+        call Fo%trace_Dphi(Dphi, .false., xo, ierr)
+        if (ierr .ne. 0) then
+           write (6, *) 'error in subroutine trace_grid: ', &
+                        'trace_Dphi returned error ', ierr
+           stop
+        endif
+
+        ! evaluate field line tracing vs. reconstruction
+        xg = Fg(it(idir),:)
+
+        ! absolute displacement
+        dx(idir) = sqrt((xg(1)-xo(1))**2 + (xg(2)-xo(2))**2)
+
+        ! radial and poloidal displacement
+        dPsidR = get_DPsiN(xo, 1, 0)
+        dPsidZ = get_DPsiN(xo, 0, 1)
+        Psig   = get_PsiN(xg)
+        Psio   = get_PsiN(xo)
+        dPsi   = Psig - Psio
+        dr(idir) = dPsi / sqrt(dPsidR**2 + dPsidZ**2)
+
+        thetag = get_poloidal_angle(xg)
+        thetao = get_poloidal_angle(xo)
+        dtheta = thetao - thetag; if (abs(dtheta) > pi) dtheta = dtheta - sign(pi2,dtheta)
+        dt(idir) = dtheta
+     enddo
+     D%x(i,1)    = get_poloidal_angle(xi)
+     D%x(i,2)    = dx(-1)
+     D%x(i,3)    = dx( 1)
+     D%x(i,4)    = dr(-1)
+     D%x(i,5)    = dr( 1)
+     D%x(i,6)    = dt(-1)
+     D%x(i,7)    = dt( 1)
+     G1%x(i,1) = ip_list(i)
+     G1%x(i,2) = ir_list(i)
+     G2%x(i,1) = D%x(i,1) / pi * 180.d0
+     G2%x(i,2) = get_PsiN(xi)
+  enddo
+  call D%plot(filename=Output_File)
+  call G1%store(filename='cell_indices.dat')
+  call G2%store(filename=Grid_File)
+
+  deallocate (ip_list, ir_list)
+  deallocate (Fg)
+ 1000 format(3x,'- Running grid accuracy check')
+  end subroutine grid_accuracy
+!=======================================================================
+
+
+
+!=======================================================================
+  subroutine flux_conservation()
+  use iso_fortran_env
+  use emc3_grid
+  use dataset
+  use grid
+  implicit none
+
+  real(real64), dimension(:,:),   allocatable :: div, pitch
+  real(real64), dimension(:,:,:), allocatable :: NL
+
+  type(t_grid)    :: G
+  type(t_dataset) :: D
+  integer         :: iz, it0, i, j, ig, n
+
+
+  write (6, 1000)
+  call load_emc3_grid()
+  call load_bfstren()
+
+  iz  = 0
+  it0 = ZON_TORO(iz) / 2
+  allocate (div  (0:ZON_POLO(iz)-1, 0:ZON_RADI(iz)-1), &
+            pitch(0:ZON_POLO(iz)-1, 0:ZON_RADI(iz)-1), &
+            NL   (0:ZON_POLO(iz)-1, 0:ZON_RADI(iz)-1, 0:ZON_TORO(iz)))
+  call check_mesh(iz, div, NL, pitch)
+
+  n = (P_SURF_PL_TRANS_RANGE(2,iz) - P_SURF_PL_TRANS_RANGE(1,iz)) * &
+      (R_SURF_PL_TRANS_RANGE(2,iz) - R_SURF_PL_TRANS_RANGE(1,iz))
+  call D%new(n, 5)
+  call G%new(LOCAL, UNSTRUCTURED, FIXED_COORD3, n)
+  ig = 0
+  do j=P_SURF_PL_TRANS_RANGE(1,iz),P_SURF_PL_TRANS_RANGE(2,iz)-1
+     do i=R_SURF_PL_TRANS_RANGE(1,iz),R_SURF_PL_TRANS_RANGE(2,iz)-1
+        ig = ig + 1
+        G%x(ig,1) = j
+        G%x(ig,2) = i
+        D%x(ig,1) = div(j,i) * 100.d0
+        D%x(ig,2) = pitch(j,i)
+        D%x(ig,3) = NL(j,i,0)
+        D%x(ig,4) = NL(j,i,it0)
+        D%x(ig,5) = NL(j,i,ZON_TORO(iz))
+     enddo
+  enddo
+  call D%plot(filename=Output_File)
+  call G%store(filename=Grid_File)
+
+
+  deallocate (div, pitch, NL)
+ 1000 format(3x,'- Running flux conservation and non-linearity check')
+  end subroutine flux_conservation
 !=======================================================================
 
 end subroutine FLR_analysis
