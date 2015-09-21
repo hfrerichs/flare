6500f3aed5eb16f92a5f38fad77d36e455c28d01
Heinke Frerichs <hfrerichs@wisc.edu>
2015-09-21 09:44:52 -0500

    -bugfix for calculation of PsiN_in with blocks > 1

--- src/fieldline_grid/inner_boundary.f90
+++ src/fieldline_grid/inner_boundary.f90
@@ -83,6 +83,7 @@ module inner_boundary
      DPsiN1(iblock,0) = sqrt(DPsiN1(iblock,0) / (n - 1.d0))
      write (6, 1002), iblock, PsiN1(iblock), DPsiN1(iblock,0), DPsiN1(iblock,-1), DPsiN1(iblock,1)
   enddo
+  PsiN_in = PsiN_in / blocks
 
  1000 format (3x,'- Inner simulation boundaries:')
  1001 format (8x,'Block   PsiN_mean                Min(PsiN)   Max(PsiN)')
