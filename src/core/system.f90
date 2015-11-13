a77444d27fa393a6fb7e00857d29776fd149bbea
Heinke Frerichs <hfrerichs@wisc.edu>
2015-11-13 09:24:32 -0600

    -add subroutine progress: log progress to screen in steps of delta

--- src/core/system.f90
+++ src/core/system.f90
@@ -1,3 +1,4 @@
+!=======================================================================
 module system
   use iso_fortran_env
   implicit none
@@ -5,5 +6,33 @@ module system
   real(real64), parameter :: &
      epsilon_r64 = epsilon(real(1.0,real64)), &
      machine_precision = epsilon_r64
-
 end module system
+!=======================================================================
+
+
+
+
+
+!=======================================================================
+subroutine progress(i, n, delta)
+  use iso_fortran_env
+  implicit none
+  integer, intent(in)      :: i, n
+  real(real64), intent(in) :: delta
+
+  integer, save :: D
+  real(real64)  :: F
+
+
+  if (i == 0) D = 0
+
+  F = 1.d0 * i / n
+  if (F >= D*delta  .or.  i==n) then
+     !write (6, *) int(F*100), i, n
+     write (6, 1000) int(F*100)
+     D = D + 1
+  endif
+
+ 1000 format(8x,i3,' %')
+end subroutine progress
+!=======================================================================
