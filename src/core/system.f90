aafee2e8eb998de6e790f6013553143a2d5347f6
Heinke Frerichs <hfrerichs@wisc.edu>
2015-12-03 12:41:14 -0600

    -modify screen output of progess to f0.2

--- src/core/system.f90
+++ src/core/system.f90
@@ -17,8 +17,9 @@ end module system
 subroutine progress(i, n, delta)
   use iso_fortran_env
   implicit none
-  integer, intent(in)      :: i, n
+  integer,      intent(in) :: i, n
   real(real64), intent(in) :: delta
+  !integer,      intent(in), optional :: incr
 
   integer, save :: D
   real(real64)  :: F
@@ -29,10 +30,12 @@ subroutine progress(i, n, delta)
   F = 1.d0 * i / n
   if (F >= D*delta  .or.  i==n) then
      !write (6, *) int(F*100), i, n
-     write (6, 1000) int(F*100)
+     !write (6, 1000) int(F*100)
+     write (6, 2000) F*100
      D = D + 1
   endif
 
  1000 format(8x,i3,' %')
+ 2000 format(8x,f0.2,' %')
 end subroutine progress
 !=======================================================================
