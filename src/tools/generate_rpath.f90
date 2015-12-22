2e6cec3732e608963f35c9fd7e3abd7dda8a0e3f
Heinke Frerichs <hfrerichs@wisc.edu>
2015-12-22 11:29:49 -0600

    -generate rpath from any given point (x_start)

--- src/tools/generate_rpath.f90
+++ src/tools/generate_rpath.f90
@@ -2,7 +2,8 @@
 ! Generate path across magnetic surfaces
 !
 ! Input (taken from run control file):
-!    N_psi              Start at X-point number N_psi
+!    N_psi              Start at X-point number N_psi, or
+!    x_start            start at this point if N_psi = 0
 !    N_theta            Direction (1: left SOL, 2: right SOL, 3: core, 4: PFR)
 !    Limit              Length of path from X-point
 !    N_steps            Number of discretization points used for Grid_File
@@ -11,7 +12,7 @@
 !===============================================================================
 subroutine generate_rpath
   use iso_fortran_env
-  use run_control, only: Grid_File, Output_File, N_theta, N_steps, N_psi, Limit
+  use run_control, only: Grid_File, Output_File, N_theta, N_steps, N_psi, x_start, Limit
   use xpaths
   use grid
   implicit none
@@ -23,7 +24,11 @@ subroutine generate_rpath
 
 
   direction = max(1, N_theta)
-  call rpath%generateX(N_psi, direction, LIMIT_LENGTH, Limit, SAMPLE_LENGTH)
+  if (N_psi == 0) then
+     call rpath%generate(x_start, direction, LIMIT_LENGTH, Limit, sampling=SAMPLE_LENGTH)
+  else
+     call rpath%generateX(N_psi, direction, LIMIT_LENGTH, Limit, SAMPLE_LENGTH)
+  endif
   call rpath%plot(filename=Output_File)
 
   if (N_steps == 0) return
