e4b5a1058261e1979d71fecddd10aab0f175e7d0
Heinke Frerichs <hfrerichs@wisc.edu>
2016-01-06 10:21:04 -0600

    -additional output in case of error

--- src/core/curve2D.F90
+++ src/core/curve2D.F90
@@ -1443,14 +1443,17 @@ module curve2D
   ! 1. check input
   if (xiA < 0.d0  .or.  xiA > 1.d0) then
      write (6, *) 'error in t_curve%split3: xiA must be in [0,1]!'
+     write (6, *) 'xiA = ', xiA
      stop
   endif
   if (xiB < 0.d0  .or.  xiB > 1.d0) then
      write (6, *) 'error in t_curve%split3: xiB must be in [0,1]!'
+     write (6, *) 'xiB = ', xiB
      stop
   endif
   if (xiA > xiB) then
      write (6, *) 'error in t_curve%split3: xiA > xiB not allowed!'
+     write (6, *) 'xiA, xiB = ', xiA, xiB
      stop
   endif
 
