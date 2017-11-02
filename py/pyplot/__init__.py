eed8125a1e90e55ccba1cd3f74a19069418c7dbe
Heinke Frerichs <hfrerichs@wisc.edu>
2017-11-02 14:53:46 -0500

    Add plot1d module (for data files containing curves and data points)

--- py/pyplot/__init__.py
+++ py/pyplot/__init__.py
@@ -1,1 +1,2 @@
 from plot_lc import plot_lc
+from plot_1d import plot_1d
---
+++ py/pyplot/plot_1d.py
@@ -0,0 +1,12 @@
+import numpy             as np
+import matplotlib.pyplot as plt
+
+
+def plot_1d(data_file, *args, **kwargs):
+    # load data file
+    d = np.loadtxt(data_file, dtype='float')
+    if d.ndim != 2:
+        print "error: unexpected dimension of data array!"
+        return
+
+    plt.plot(d[:,0], d[:,1], *args, **kwargs)
