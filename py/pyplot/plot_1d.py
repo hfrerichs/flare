57e08499592a803422a54dc61c7cf400dba96d6d
Heinke Frerichs <hfrerichs@wisc.edu>
2017-11-07 08:38:54 -0600

    Add keyword fill for switching between plt.plot and plt.fill

--- py/pyplot/plot_1d.py
+++ py/pyplot/plot_1d.py
@@ -4,11 +4,20 @@ import matplotlib.pyplot as plt
 
 
 def plot_1d(data_file, *args, **kwargs):
-    if not os.path.isfile(data_file): return
-    # load data file
+    # check if data file exists
+    if not os.path.isfile(data_file):
+        print "error: data file {} not found!".format(data_file)
+        return
+
+    # load data file and check dimensions
     d = np.loadtxt(data_file, dtype='float')
     if d.ndim != 2:
         print "error: unexpected dimension of data array!"
         return
 
-    plt.plot(d[:,0], d[:,1], *args, **kwargs)
+    fill = kwargs.get('fill', False)
+
+    if fill:
+        plt.fill(d[:,0], d[:,1], *args, **kwargs)
+    else:
+        plt.plot(d[:,0], d[:,1], *args, **kwargs)
