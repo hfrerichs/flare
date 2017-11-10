0ad3846dd3651209026baae80e4f3efe29ff3000
Heinke Frerichs <hfrerichs@wisc.edu>
2017-11-10 14:13:24 -0600

    Add optional input of machine and configuration for loading configuration from database

--- src/interface/backend.f90
+++ src/interface/backend.f90
@@ -1,11 +1,17 @@
-subroutine init()
-  use run_control, only: load_run_control
+subroutine init(machine, configuration)
+  use run_control, only: load_run_control, data_dir, Prefix, Bfield_input_file
   use bfield
   use boundary
   implicit none
+  character(len=*), intent(in), optional :: machine, configuration
 
 
-  call load_run_control('run.conf')
+  if (present(machine) .and. present(configuration)) then
+     Prefix = data_dir//'/'//trim(machine)//'/'//trim(configuration)//'/'
+     Bfield_input_file = trim(Prefix)//'bfield.conf'
+  else
+     call load_run_control('run.conf')
+  endif
   call setup_bfield_configuration()
   call setup_boundary()
 
