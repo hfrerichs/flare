61b95351fb3fd861154e1b7bb1225d32dc89c6aa
Heinke Frerichs <hfrerichs@wisc.edu>
2019-09-30 08:36:18 -0500

    Change argument order for data_file and data_key

--- bin/flare-plot.py
+++ bin/flare-plot.py
@@ -11,11 +11,11 @@ import flare.plot_data
 #===============================================================================
 def plot():
     parser = argparse.ArgumentParser(prog="flare plot")
+    parser.add_argument("data_file")
     parser.add_argument("data_key", help="data key as listed by 'flare info'")
     parser.add_argument("-g", "--geometry_file", help="alternative geometry file")
     plot_functions = ['tricontourf', 'scipy_griddata_pcontourf', 'matplotlib_griddata_pcontourf']
     parser.add_argument("-pf", "--plot_function", choices=plot_functions, default=plot_functions[0], help="select plot function for unstructured data")
-    parser.add_argument("data_file")
     parser.add_argument("-D", "--dimension", type=int, help="set plot dimension")
     args   = parser.parse_args()
     flare.plot_data.plot_data(args.data_file, args.data_key, args.geometry_file, plot_function=args.plot_function, dimension=args.dimension)
